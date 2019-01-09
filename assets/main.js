892d219c0d0628e08ae32dd1dfd1791cbd692916
Lore <loretapiap@gmail.com>
2019-01-09 16:34:01 -0300

    Add rut validation

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,38 @@ new Vue({
   el: '#app',
    data: {
     email: '',
+    rut: '',
     reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/
   },
-    methods: {
+  computed: {},
+  methods: {
     isEmailValid: function() {
       return (this.email == "")? "" : (this.reg.test(this.email)) ? 'has-success' : 'has-error';
+    },
+    isRutValid: function() {
+        return (this.rut == "")? "" : (Fn.validateRut(this.rut)) ? 'has-success' : 'has-error';
     }
   }
 });
+
+// rut validation
+
+var Fn = {
+	validateRut : function (newRut) {
+		newRut = newRut.replace("‐","-");
+		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( newRut ))
+			return false;
+		var tmp 	= newRut.split('-');
+		var digv	= tmp[1];
+		var rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+
+		return (Fn.dv(rut) == digv );
+	},
+	dv : function(T){
+		var M=0,S=1;
+		for(;T;T=Math.floor(T/10))
+			S=(S+T%10*(9-M++%6))%11;
+		return S?S-1:'k';
+	}
+}
